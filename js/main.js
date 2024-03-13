d43c29590407cbc7eb32e458c1966fb1f20909f1
ttainam <tainamgbn@gmail.com>
2024-03-13 13:12:27 -0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-      var targetDateGMT = new Date('2024-03-28T00:00:00Z');
-      var currentDateGMT = new Date();
-      var timeDifference = currentDateGMT.getTimezoneOffset() * 60 * 1000;
-      var targetDateAdjusted = new Date(targetDateGMT.getTime() + timeDifference);
-      var twoDaysFromNow = (targetDateAdjusted.getTime() / 1000) + (86400);
+  var targetDateGMT = new Date('2024-03-28T00:00:00Z');
+  var currentDateGMT = new Date();
+  var timeDifference = currentDateGMT.getTimezoneOffset() * 60 * 1000;
+  var targetDateAdjusted = new Date(targetDateGMT.getTime() + timeDifference);
+  var twoDaysFromNow = (targetDateAdjusted.getTime() / 1000) + (86400);
 
-      var flipdown = new FlipDown(twoDaysFromNow)
-        .start()
-        .ifEnded(() => {
-          console.log('O contador chegou ao fim!');
-        });
+  var flipdown = new FlipDown(twoDaysFromNow)
+    .start()
+    .ifEnded(() => {
+      console.log('O contador chegou ao fim!');
+    });
 
-      var interval = setInterval(() => {
-        let body = document.body;
-        body.classList.toggle('light-theme');
-        body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
-        body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
-      }, 5000);
+  var interval = setInterval(() => {
+    let body = document.body;
+    body.classList.toggle('light-theme');
+    body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
+    body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
+  }, 5000);
 
-  // Show version number
-  var ver = document.getElementById('ver');
-  ver.innerHTML = flipdown.version;
+// Show version number
+var ver = document.getElementById('ver');
+ver.innerHTML = flipdown.version;
 });
