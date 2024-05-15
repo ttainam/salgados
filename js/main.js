5521fd7243ae930fd7785ca446d331f44706ab85
ttainam <tainamgbn@gmail.com>
2024-05-15 13:30:42 -0300

    fix

--- js/main.js
+++ js/main.js
@@ -233,7 +233,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('meuBotao').addEventListener('click', function(event) {
     event.preventDefault();
     localStorage.setItem('_cart', JSON.stringify(dados));
-    window.location.href = "https://deliciasdagrazzi.qrpedir.com/"
+    window.open("https://deliciasdagrazzi.qrpedir.com/","_blank")
   });
 });
 
